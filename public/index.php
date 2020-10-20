8986039fd8ffafc3a67dcce7f4339516f258a3e6
Aurélie Fqt <70887548+AurelieFqt@users.noreply.github.com>
2020-10-20 12:19:23 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 use App\Wcs\Hello;
+use HelloWorld\SayHello;
 
 $bonjour = new Hello();
 $bonjour->talk();
+
+$sayHello = new SayHello();
+echo $sayHello->world();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,22 @@ class ComposerStaticInit0d188091926f93a65e3b1b78c8205b84
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HelloWorld' => 
+            array (
+                0 => __DIR__ . '/..' . '/ehime/hello-world/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit0d188091926f93a65e3b1b78c8205b84::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit0d188091926f93a65e3b1b78c8205b84::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit0d188091926f93a65e3b1b78c8205b84::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
